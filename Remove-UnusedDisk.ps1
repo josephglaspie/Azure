d093f11f0c4bd3dfb698c3e02f2b1f5639c510dc
Joseph <joseph_glaspie@homedepot.com>
2018-09-04 20:42:40 -0500

    initial

---
+++ Remove-UnusedDisk.ps1
@@ -0,0 +1,14 @@
+﻿$unUsedDisks = Import-Csv '/Users/jxg2980/Downloads/disks_6-5.csv'   
+$interlineSubs = Get-AzureRmSubscription | where-object {$_.Name -like "*interline*"} 
+
+ForEach($interlineSub in $interlineSubs){
+    
+    foreach($unUsedDisk in $unUsedDisks){
+        
+        if($unUsedDisk.subscription -eq $interlineSub.name){
+           $disk = Get-AzureRmDisk | Where-Object {$_.Name -eq $unUsedDisk.name} 
+           $disk | Remove-AzureRmDisk -Confirm:0
+           Write-Verbose "Deleting Disk $($unusedDisk.name) on Sub: $($interlineSub.name)" -Verbose
+        }
+    }
+}
